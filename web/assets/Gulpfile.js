218c804293163caa934335cb5912d366d6993075
Felds Liscia <dev@felds.com.br>
2016-05-09 16:38:15 -0300

    fix gulp

--- web/assets/Gulpfile.js
+++ web/assets/Gulpfile.js
@@ -9,10 +9,6 @@ const livereload = require('gulp-livereload');
 // CSS
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
-// JS
-const source = require('vinyl-source-stream');
-const browserify = require('browserify');
-const babelify = require('babelify');
 
 
 
@@ -44,41 +40,14 @@ gulp.task('sass', [],  function () {
     ;
 });
 
-const get_browserify_task_func = (in_file, out_file) => () => {
-    return browserify({
-        entries: [ `./js-source/${in_file}` ],
-        debug: false,
-    })
-        // stage 0 = the awesomest stuff out there
-        // @link http://babeljs.io/docs/plugins/preset-stage-0/
-        // @link http://giphy.com/gifs/badass-fHR0X5PiXsUSY
-        .transform("babelify", {
-            presets: [
-                "es2015",
-                "react",
-                "stage-0",
-            ],
-        })
-        .bundle()
-        .on('error', handle_error)
-        .pipe(source(out_file))
-        .pipe(gulp.dest('js/'))
-        .pipe(livereload())
-    ;
-};
-gulp.task('browserify-dashboard', [], get_browserify_task_func('dashboard.jsx', 'dashboard.bundle.js'));
-gulp.task('browserify', [
-    'browserify-dashboard',
-]);
 
 
 // Main task
 // ---------
 gulp.task('default', [
     'sass',
-    'browserify',
 ], function () {
     livereload.listen();
     gulp.watch('sass/**/*.s[ac]ss', [ 'sass' ]);
-    gulp.watch('js-source/**/*.js*', [ 'browserify-dashboard' ]);
 });
+
