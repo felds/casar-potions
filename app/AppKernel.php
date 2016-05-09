81d199323d037c9d22addf1ddcb156521565f068
Felds Liscia <dev@felds.com.br>
2016-05-09 16:27:32 -0300

    add dummy api

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -14,7 +14,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\RestBundle\FOSRestBundle(),
             new AppBundle\AppBundle(),
         ];
 
---
+++ src/AppBundle/Api/PotionController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace AppBundle\Api;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * @Route("/potion")
+ */
+class PotionController extends Controller
+{
+    /**
+     * @Route("/", name="api_potion_index")
+     * @Method("GET")
+     */
+    public function cgetAction()
+    {
+        $response = new JsonResponse($this->getPotions());
+
+        return $response;
+    }
+
+    private function getPotions()
+    {
+        $json = <<<EOF
+            {
+              "potions": {
+                "1": {
+                  "id": 1,
+                  "name": "Aging Potion",
+                  "image": "aging-potion.png",
+                  "price": 29.99,
+                  "effect": "Causes the drinker to advance in age",
+                  "ingredients": [
+                    "Red Wine",
+                    "Prune Juice",
+                    "Hairy Fungus",
+                    "Tortoise Shell",
+                    "Caterpillar",
+                    "Bat Tongue"
+                  ]
+                },
+                "2": {
+                  "id": 2,
+                  "name": "Bulgeye Potion",
+                  "image": "bulgeye-potion.png",
+                  "price": 19.99,
+                  "effect": "It affects one's eyes, causing them to swell",
+                  "ingredients": [
+                    "Beetle eyes",
+                    "Eel eyes"
+                  ]
+                },
+                "3": {
+                  "id": 3,
+                  "name": "Dragon Tonic",
+                  "image": "dragon-tonic.png",
+                  "price": 64.99,
+                  "effect": "A tonic used to cure sick dragons",
+                  "ingredients": [
+                    "Eagle Owl feathers",
+                    "Peacock feathers",
+                    "Giant Purple Toad warts"
+                  ]
+                },
+                "4": {
+                  "id": 4,
+                  "name": "Love Potion",
+                  "image": "love-potion.png",
+                  "price": 29.99,
+                  "effect": "The one who drinks it falls deeply in love with the first person they see",
+                  "ingredients": [
+                    "Petals from a red rose",
+                    "Essence of violet",
+                    "Canary flight and down feathers",
+                    "Newt eyes"
+                  ]
+                },
+                "5": {
+                  "id": 5,
+                  "name": "Polyjuice Potion",
+                  "image": "polyjuice-potion.png",
+                  "price": 99.99,
+                  "effect": "Allows the drinker to assume the form of someone else",
+                  "ingredients": [
+                    "Lacewing flies",
+                    "Leeches",
+                    "Powdered bicorn horn",
+                    "Knotgrass",
+                    "Fluxweed",
+                    "Shredded Boomslang skin"
+                  ]
+                },
+                "6": {
+                  "id": 6,
+                  "name": "Sleeping Draught",
+                  "image": "sleeping-draught.png",
+                  "price": 29.99,
+                  "effect": "Causes the drinker to fall almost instantaneously into a deep but temporary sleep",
+                  "ingredients": [
+                    "Sprigs of Lavender",
+                    "Measures of Standard Ingredient",
+                    "Blobs of Flobberworm Mucus",
+                    "Valerian sprigs"
+                  ]
+                }
+              }
+            }
+EOF;
+        return json_decode($json);
+    }
+}
